64d0c31203ba51d1157b335580f58c0ec838aab8
donl0 <donl_117n@mail.ru>
2024-08-14 20:13:08 +0300

    Add exception

---
+++ backend/PizzaService/Infrastructure/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) not found") { }
+    }
+}
