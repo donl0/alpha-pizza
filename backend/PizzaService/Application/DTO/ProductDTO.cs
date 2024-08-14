2f347908e220298c22fafd0a15b5b8103044794d
donl0 <donl_117n@mail.ru>
2024-08-14 20:08:31 +0300

    Add DTO for save cost/size features

---
+++ backend/PizzaService/Application/DTO/ProductDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.DTO
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ backend/PizzaService/Application/DTO/SizeCostDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTO
+{
+    public class SizeCostDTO
+    {
+        public int Size { get; set; }
+        public int Cost { get; set; }
+    }
+}
