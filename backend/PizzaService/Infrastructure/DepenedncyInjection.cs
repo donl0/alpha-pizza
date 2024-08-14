5698cce91875b9ed9a744ff9447e1dc7e8ed1837
donl0 <donl_117n@mail.ru>
2024-08-14 20:13:32 +0300

    Register dependencies for other layers

--- backend/PizzaService/Infrastructure/DepenedncyInjection.cs
+++ backend/PizzaService/Infrastructure/DepenedncyInjection.cs
@@ -1,5 +1,9 @@
-﻿using Application.Interfaces;
+﻿using Application.DTO;
+using Application.Interfaces;
+using Application.Interfaces.Services;
+using Domain.Models;
 using Infrastructure.Db;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +20,12 @@ namespace Infrastructure
             {
                 options.UseNpgsql(connectionString);
             });
+
+
+            services.AddTransient<ISaveService<SizeCostDTO, SizeCost>, SizeCostSaver>();
+            services.AddTransient<ISaveService<ProductDTO, Product>, ProductCostSaver>();
+            services.AddTransient<PizzaUpdateImageService>();
+            services.AddTransient<PizzaCreateService>();
 
             return services;
         }
