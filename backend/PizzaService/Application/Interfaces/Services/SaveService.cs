20a72b5a7e7e7106fad0338110246c225875c84f
donl0 <donl_117n@mail.ru>
2024-08-14 20:06:18 +0300

    Add save service, costSaver features

---
+++ backend/PizzaService/Application/Interfaces/Services/SaveService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces.Services
+{
+    public interface ISaveService<T, V>
+    {
+        public Task<V> SaveAsync(T value);
+        public Task<List<V>> SaveAsync(ICollection<T> values);
+    }
+}
---
+++ backend/PizzaService/Infrastructure/Services/SizeCostSaver.cs
@@ -0,0 +1,54 @@
+﻿using Application.DTO;
+using Application.Interfaces;
+using Application.Interfaces.Services;
+using Domain.Models;
+
+namespace Infrastructure.Services
+{
+    internal class SizeCostSaver : ISaveService<SizeCostDTO, SizeCost>
+    {
+        private readonly IPizzaDbContext _pizzaDbContext;
+
+        private readonly CancellationToken _cancellationToken = new CancellationToken();
+
+        public SizeCostSaver(IPizzaDbContext pizzaDbContext)
+        {
+            _pizzaDbContext = pizzaDbContext;
+        }
+
+        public async Task<SizeCost> SaveAsync(SizeCostDTO value)
+        {
+            SizeCost sizeCost = new SizeCost
+            {
+                Size = value.Size,
+                Cost = value.Cost
+            };
+
+            await _pizzaDbContext.SizeCosts.AddAsync(sizeCost);
+            await _pizzaDbContext.SaveChangesAsync(_cancellationToken);
+
+            return sizeCost;
+        }
+
+        public async Task<List<SizeCost>> SaveAsync(ICollection<SizeCostDTO> values)
+        {
+            List<SizeCost> sizeCosts = new List<SizeCost>();
+
+            foreach (var item in values)
+            {
+                SizeCost sizeCost = new SizeCost
+                {
+                    Size = item.Size,
+                    Cost = item.Cost
+                };
+
+                sizeCosts.Add(sizeCost);
+            }
+
+            await _pizzaDbContext.SizeCosts.AddRangeAsync(sizeCosts);
+            await _pizzaDbContext.SaveChangesAsync(_cancellationToken);
+
+            return sizeCosts;
+        }
+    }
+}
