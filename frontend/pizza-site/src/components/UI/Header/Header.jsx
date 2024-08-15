a188868e6fd0ea7f35540b4e6fbf3b93db96d646
donl0 <donl_117n@mail.ru>
2024-08-15 17:38:38 +0300

    Clear header by subtituting HighlitedLinks component

--- frontend/pizza-site/src/components/UI/Header/Header.jsx
+++ frontend/pizza-site/src/components/UI/Header/Header.jsx
@@ -1,67 +1,26 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import classes from './Header.module.css';
+import HighlitedLinks from '../HighlitedLinks';
 
-let previousActiveLink = ""
+const showObjects = {
+    "/page": "",
+    "/menu": "MENU",
+    "/restaurant" : "RESTAURANT",
+    "/offer": "OFFER YOUR PIZZA"
+}
+
+const additionalObjects = {
+    "/page" : () => { return <img src="/header/Badge.png" alt="Badge" className={classes.headerImage} />} 
+}
 
 const Header = () => {
-    const [activeLink, setActiveLink] = useState(null);
-
-    
-    const handleLinkClick = (link) => {
-        previousActiveLink = activeLink
-        setActiveLink(link)
-    };
-
-    const handleLinkEnter = (link) => {
-        setActiveLink(link)
-    };
-
-    const handleLinkOver = (link) => {
-        if (previousActiveLink !== link) {
-            setActiveLink(previousActiveLink);
-        }
-    };
-
-    const active_state = classes.active;
-
     return (
         <div className={classes.header}>
-            <Link to="/page" className={classes.headerImageLink}>
-                <img src="/header/Badge.png" className={`${classes.headerImage} ${activeLink === 'page' ? active_state : ''}`} alt="Badge" 
-                onClick={() => handleLinkClick('page')}
-                onMouseEnter={() => handleLinkEnter('page')}
-                onMouseOut={() => handleLinkOver('page')}/>
-            </Link>
-            <div className={classes.textContainer}>
-                <Link 
-                    to="/menu"
-                    className={`${classes.textContainer__text} ${classes.text} ${activeLink === 'menu' ? active_state : ''}`}
-                    onClick={() => handleLinkClick('menu')}
-                    onMouseEnter={() => handleLinkEnter('menu')}
-                    onMouseOut={() => handleLinkOver('menu')}
-                >
-                    MENU
-                </Link>
-                <Link 
-                    to="/restaurants"
-                    className={`${classes.textContainer__text} ${classes.text} ${activeLink === 'restaurants' ? active_state : ''}`}
-                    onClick={() => handleLinkClick('restaurants')}
-                    onMouseEnter={() => handleLinkEnter('restaurants')}
-                    onMouseOut={() => handleLinkOver('restaurants')}
-                >
-                    RESTAURANTS
-                </Link>
-                <Link 
-                    to="/offer"
-                    className={`${classes.textContainer__text} ${classes.text} ${activeLink === 'offer' ? active_state : ''}`}
-                    onClick={() => handleLinkClick('offer')}
-                    onMouseEnter={() => handleLinkEnter('offer')}
-                    onMouseOut={() => handleLinkOver('offer')}
-                >
-                    OFFER YOUR PIZZA
-                </Link>
-            </div>
+            <HighlitedLinks linkInscriptionDictionary={showObjects} 
+            additionalObjects={additionalObjects} 
+            activeClass="active" 
+            defaultClass={`${classes.textContainer__text} ${classes.text}`} 
+            containerClass={classes.textContainer}></HighlitedLinks>
         </div>
     );
 };
