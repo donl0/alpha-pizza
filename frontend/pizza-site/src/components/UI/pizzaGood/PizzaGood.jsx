826687236eafb77eaeb99632ecdf676b0c644ce1
donl0 <donl_117n@mail.ru>
2024-08-16 21:00:27 +0300

    Updaye pizza good layput

--- frontend/pizza-site/src/components/UI/pizzaGood/PizzaGood.jsx
+++ frontend/pizza-site/src/components/UI/pizzaGood/PizzaGood.jsx
@@ -1,21 +1,18 @@
 import styles from "./PizzaGood.module.css"
 import OrangeSecondaryActionButton from "./../Buttons/FadeActionButton/OrangeSecondaryActionButton"
 
-const PizzaGood = ({image, name, prise, buttonCaption}) => {
+const PizzaGood = ({image, name, prise, buttonCaption, id}) => {
     return (
         <div className={styles.pizzaGood__container}>
             <div className={styles.pizzaGood__container__image}>
                 <img src={image} className={styles.pizzaGood__image}></img>
             </div>
-            <div>
-                <span className={styles.pizzaGood__mainText}>{name}</span>
+            <span className={styles.pizzaGood__mainText}>{name}</span>
                 
                 <div className={styles.pizzaGood__bottomLineContainer}>
                     <span className={styles.pizzaGood__prise}>from {prise}$</span>
                     <OrangeSecondaryActionButton>{buttonCaption}</OrangeSecondaryActionButton>
                 </div>
-                
-            </div>
             
         </div>
     )
