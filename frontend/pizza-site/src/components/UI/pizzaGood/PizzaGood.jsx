abf2fa51bb1933599881a7ed08d7c329af04a85a
donl0 <donl_117n@mail.ru>
2024-08-16 20:27:52 +0300

    Add pizza template component

---
+++ frontend/pizza-site/src/components/UI/pizzaGood/PizzaGood.jsx
@@ -0,0 +1,24 @@
+import styles from "./PizzaGood.module.css"
+import OrangeSecondaryActionButton from "./../Buttons/FadeActionButton/OrangeSecondaryActionButton"
+
+const PizzaGood = ({image, name, prise, buttonCaption}) => {
+    return (
+        <div className={styles.pizzaGood__container}>
+            <div className={styles.pizzaGood__container__image}>
+                <img src={image} className={styles.pizzaGood__image}></img>
+            </div>
+            <div>
+                <span className={styles.pizzaGood__mainText}>{name}</span>
+                
+                <div className={styles.pizzaGood__bottomLineContainer}>
+                    <span className={styles.pizzaGood__prise}>from {prise}$</span>
+                    <OrangeSecondaryActionButton>{buttonCaption}</OrangeSecondaryActionButton>
+                </div>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default PizzaGood;
