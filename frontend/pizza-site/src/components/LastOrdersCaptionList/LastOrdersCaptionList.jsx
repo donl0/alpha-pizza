0f73684b95e61c6dbbf1850aca56262cc98f6177
donl0 <donl_117n@mail.ru>
2024-08-16 13:16:39 +0300

    Add last orders in layout

--- frontend/pizza-site/src/components/LastOrdersCaptionList/LastOrdersCaptionList.jsx
+++ frontend/pizza-site/src/components/LastOrdersCaptionList/LastOrdersCaptionList.jsx
@@ -1,10 +1,64 @@
 import OrangeCaption from "../UI/Caption/OrangeCaption"
+import LastOrdersList from "../UI/LastOrdersList/LastOrdersList";
 import styles from './LastOrdersCaptionList.module.css';
 
 const LastOrdersCaptionList = ({children}) => {
+    const pizzas = [{
+        name: "pineApple",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    },{
+        name: "barkeku",
+        image: 'pineApple.png'
+    }
+    ]
+
     return (
-        <div className={styles.container}>
-            <OrangeCaption>{children}</OrangeCaption>
+        <div >
+            <div className={styles.container}>
+                <OrangeCaption>{children}</OrangeCaption>
+            </div>
+            <div>
+                <LastOrdersList pizzas={pizzas}></LastOrdersList>
+            </div>
         </div>
     );
 }
