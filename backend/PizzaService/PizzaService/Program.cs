366cab797ba1b8b5937d881f62377db11c6edb36
donl0 <donl_117n@mail.ru>
2024-08-17 23:44:59 +0300

    Register Image saver

--- backend/PizzaService/PizzaService/Program.cs
+++ backend/PizzaService/PizzaService/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure;
+using PizzaService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddTransient<IImageSaver, ImageSaver>();
 
 var app = builder.Build();
 
