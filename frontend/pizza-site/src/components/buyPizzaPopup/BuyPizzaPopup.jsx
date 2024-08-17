0336ad6202e58f400e9e2bc3b648da1a0dd1d82b
donl0 <donl_117n@mail.ru>
2024-08-17 19:12:30 +0300

    Move chose ingridients logic to another responsobility from popup

--- frontend/pizza-site/src/components/buyPizzaPopup/BuyPizzaPopup.jsx
+++ frontend/pizza-site/src/components/buyPizzaPopup/BuyPizzaPopup.jsx
@@ -1,21 +1,5 @@
-import PizzaInfoBlock from "../UI/pizzaInfoBlock/PizzaInfoBlock"
 import styles from "./BuyPizzaPopup.module.css"
-
-const pizza = {
-    name: "Pizza Picanta with feta",
-    consistOf: ["Ham", "feta cheese", "champignons", "red onion", "arugula", "mozzarella cheese", "tomato sauce"],
-    sizes: [{
-        size: 23,
-        cost: 500,
-    },
-    {
-        size:30,
-        cost:655
-    },
-    {size:36,
-        cost:800
-    }]
-}
+import PizzaAllIngridientsBlock from "./pizzaAllIngridientsBlock/PizzaAllIngridientsBlock"
 
 const BuyPizzaPopup = ({isActive, setPopupState}) =>{
     function stopNextPropagation(event){
@@ -30,10 +14,7 @@ const BuyPizzaPopup = ({isActive, setPopupState}) =>{
                 <div className={styles.pizyPopup__menu__pizzaContainer}>
                     <img src="/images/pizzas/custom_pizza.svg" className={styles.manImage}></img>
                 </div>
-                <div className={styles.pizyPopup__menu__itemsContainer}>
-                    <PizzaInfoBlock name={pizza["name"]}
-                    consistOf={pizza["consistOf"]}></PizzaInfoBlock>
-                </div>
+                <PizzaAllIngridientsBlock></PizzaAllIngridientsBlock>
             </div>
         </div>
     )
