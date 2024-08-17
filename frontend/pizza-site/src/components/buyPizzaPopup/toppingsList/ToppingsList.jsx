e0fc5193ad265a74389026c51fb2c202acabdce3
donl0 <donl_117n@mail.ru>
2024-08-17 19:11:29 +0300

    Add list to handle buying elements

---
+++ frontend/pizza-site/src/components/buyPizzaPopup/toppingsList/ToppingsList.jsx
@@ -0,0 +1,72 @@
+import ToppingItem from "./toppingItem/ToppingItem";
+import styles from "./ToppingsList.module.css"
+
+const toppings = [{
+id:1,
+price: 14,
+image: "/images/pizzas/custom_pizza.svg",
+name: "mozzarella with cheese asd"
+},{
+id:2,
+price: 14,
+image: "/images/pizzas/custom_pizza.svg",
+name: "cheez"
+},{
+    id:3,
+    price: 14,
+    image: "/images/pizzas/custom_pizza.svg",
+    name: "cheez"
+    },{
+        id:4,
+        price: 14,
+        image: "/images/pizzas/custom_pizza.svg",
+        name: "cheez"
+        },{
+            id:5,
+            price: 14,
+            image: "/images/pizzas/custom_pizza.svg",
+            name: "cheez"
+            },{
+                id:5,
+                price: 14,
+                image: "/images/pizzas/custom_pizza.svg",
+                name: "cheez"
+                },{
+                    id:5,
+                    price: 14,
+                    image: "/images/pizzas/custom_pizza.svg",
+                    name: "cheez"
+                    },{
+                        id:5,
+                        price: 14,
+                        image: "/images/pizzas/custom_pizza.svg",
+                        name: "cheez"
+                        },{
+                            id:5,
+                            price: 14,
+                            image: "/images/pizzas/custom_pizza.svg",
+                            name: "cheez"
+                            }]
+
+const ToppingsList = () => {
+    return (
+        <div>
+            <span className={styles.toppingItem__name}>Toppings</span>
+            <div className={styles.toppings__toppingsContainer}>
+                {toppings.map( (topping) => {
+                    return (
+                        <ToppingItem key={topping["id"]}
+                        image={topping["image"]}
+                        name={topping["name"]}
+                        price={topping["price"]}>
+                        
+                        </ToppingItem>
+                    )
+                })}
+                
+            </div>
+        </div>
+    )
+}
+
+export default ToppingsList;
