7a945deb4f6119bcfcf8fc0336d2b925870116f2
donl0 <donl_117n@mail.ru>
2024-08-17 23:45:54 +0300

    Add toppings controller

---
+++ backend/PizzaService/PizzaService/DTOs/ToppingDTO.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+
+namespace PizzaService.DTOs
+{
+    public class ToppingDTO : Product
+    {
+        public int Price { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
