e50fa8aaaf91dda6339d6658bcf655bacd67caa3
donl0 <donl_117n@mail.ru>
2024-08-17 19:10:49 +0300

    Add topping item with counter

---
+++ frontend/pizza-site/src/components/buyPizzaPopup/toppingsList/toppingItem/ToppingItem.jsx
@@ -0,0 +1,51 @@
+import styles from "./ToppingItem.module.css"
+import React, { useState } from 'react';
+
+const ToppingItem = ({image, name, price}) => {
+    const [count, setCount] = useState(0);
+
+    function increase(){
+        setCount(count + 1);
+    }
+
+    function decrease(){
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+    return (
+        <div className={styles.toppingItem__container}>
+            <div className={styles.toppingItem__container__imageContainer}>
+                <img src={image} className={styles.imageContainer__image}></img>
+            </div>
+          <div className={styles.toppingItem__textContainer}>
+            <span >{name}</span>           
+          </div>
+          <div className={styles.priseContainer}>
+                <span>{price}$</span>
+            </div>
+          <div className={styles.toppingItem__countContainer}>
+            
+          <div className={`${styles.counter}`}>
+            <div
+            className={styles.counter__decreaser}
+            onClick={decrease}
+            >-</div>
+
+            <span>{count}</span>
+
+            <div 
+            className={styles.counter__increaser}
+            onClick={increase}
+            >+</div>
+
+            </div>
+            
+          </div>
+
+
+        </div>
+    )
+}
+
+export default ToppingItem;
