d015001d2d994638a4355474fa61dd9564f8cb8f
donl0 <donl_117n@mail.ru>
2024-08-18 16:07:05 +0300

    Add product controller with get

---
+++ backend/PizzaService/PizzaService/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PizzaService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IPizzaDbContext _context;
+
+        private readonly CancellationToken _token = new CancellationToken();
+
+        public ProductsController(IPizzaDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            List<Product> products = await _context.Products.AsNoTracking().ToListAsync();
+
+            if (products.Count > 0)
+            {
+                return Ok(products);
+            }
+
+            return NotFound();
+        }
+    }
+}
