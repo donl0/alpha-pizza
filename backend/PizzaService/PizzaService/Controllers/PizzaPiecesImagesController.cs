cdffc4e3d9a6168f5f00e051887089175cb4ddee
donl0 <donl_117n@mail.ru>
2024-08-20 23:56:17 +0300

    Add pieces images controller with get all and get with image id

---
+++ backend/PizzaService/PizzaService/Controllers/PizzaPiecesImagesController.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaPiecesImagesController : ControllerBase
+    {
+        private readonly IPizzaDbContext _context;
+
+        public PizzaPiecesImagesController(IPizzaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaPiecesImages>>> Get()
+        {
+            List<PizzaPiecesImages> result = await _context.PizzaPiecesImages.Include(p => p.Pizza).ThenInclude(p => p.ConsistOf).Include(o => o.Pizza)
+             .ThenInclude(p => p.SizeCosts).ToListAsync();
+
+            if (result.Count > 0)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("{pizzaId}")]
+        public async Task<ActionResult<PizzaPiecesImages>> Get(Guid pizzaId)
+        {
+            PizzaPiecesImages result = await _context.PizzaPiecesImages.Include(p => p.Pizza).ThenInclude(p => p.ConsistOf).Include(o => o.Pizza)
+             .ThenInclude(p => p.SizeCosts).FirstOrDefaultAsync(p => p.Pizza.Id == pizzaId);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+        }
+    }
+}
