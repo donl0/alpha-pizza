e0ce4d662848e843142287cd4b8425031cb03c3c
donl0 <donl_117n@mail.ru>
2024-08-20 22:50:20 +0300

    Add new layer for ToppingsList to make general layer lighter

--- frontend/pizza-site/src/components/UI/pizzaBuyPopup/orderSideActorContainer/OrderSideActorContainer.jsx
+++ frontend/pizza-site/src/components/UI/pizzaBuyPopup/orderSideActorContainer/OrderSideActorContainer.jsx
@@ -1,78 +1,33 @@
-import { createOrder } from "../../../../services/api/order";
-import { getAllToppings } from "../../../../services/api/topping";
-import { parseToppings } from "../../../../services/parceTopping";
-import ToppingsList from "../../../buyPizzaPopup/toppingsList/ToppingsList";
-import { CurrentSizeContext, PriseContext, ToppingsContext } from "../../../pizzaOrderPopup/PizzaOrderPopup";
+import { CurrentSizeContext, PriseContext, ToppingsCostContext } from "../../../pizzaOrderPopup/PizzaOrderPopup";
 import PinkFadeActionButton from "../../Buttons/FadeActionButton/pinkFadeActionButton/PinkFadeActionButton";
 import CustomSelect from "../../Select/CustomSelect";
+import ToppingListCalculator from "../toppingListCalculator/ToppingListCalculator";
 import styles from "./OrderSideActorContainer.module.css"
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 
-const OrderSideActorContainer = ({header, sizeCosts, costs, additionalToppingsContent, onOrderButtonClicked}) => {
-    const {finalPrise, setFinalPrise} = useContext(PriseContext);
-    const {selectedToppings, setSelectedToppings} = useContext(ToppingsContext);
+const OrderSideActorContainer = ({ header, sizeCosts, costs, additionalToppingsContent, onOrderButtonClicked }) => {
+    const { finalPrise, setFinalPrise } = useContext(PriseContext);
 
-    const [currentToppingsCost, setCurrentToppingsCost] = useState(0);
-    const {currentSize, setCurrentSize} = useContext(CurrentSizeContext)
+    const { currentSize, setCurrentSize } = useContext(CurrentSizeContext)
+    const { currentToppingsCost, setCurrentToppingsCost } = useContext(ToppingsCostContext)
 
-    useEffect( () => {
+    useEffect(() => {
         const minSize = getMinSizeFromPizzasSize();
         setCurrentSize(minSize);
     }, [])
 
-    const [toppings, setToppings] = useState([]);
 
     useEffect(() => {
-        const fetchToppings = async () => {
-            const fetchedToppings = await getAllToppings();
-            const parsedToppings = parseToppings(fetchedToppings);
-            setToppings(parsedToppings);
-        };
-
-        fetchToppings();
-    }, []);
-
-    useEffect(() => {
-        if (currentSize == undefined){
+        if (currentSize == undefined) {
             return;
         }
 
         updateFinalPrise();
     }, [currentToppingsCost, currentSize]);
 
-    function addSelectedTopping(id) {
-        setSelectedToppings(prevSelectedToppings => {
-            const newSelectedToppings = { ...prevSelectedToppings };
-            if (!newSelectedToppings.hasOwnProperty(id)) {
-                newSelectedToppings[id] = 1;
-            } else {
-                newSelectedToppings[id] += 1;
-            }
-            return newSelectedToppings;
-        });
-    }
-
-    function removeSelectedTopping(id) {
-        setSelectedToppings(prevSelectedToppings => {
-            const newSelectedToppings = { ...prevSelectedToppings };
-            if (newSelectedToppings[id] > 0) {
-                newSelectedToppings[id] -= 1;
-            }
-
-            if (newSelectedToppings[id] === 0) {
-                delete newSelectedToppings[id];
-            }
-
-            return newSelectedToppings;
-        });
-    }
 
     function updateFinalPrise() {
         setFinalPrise(getSizePrice(currentSize) + currentToppingsCost);
-    }
-
-    function handleCurrentToppingsCost(value) {
-        setCurrentToppingsCost(value);
     }
 
     function updateSize(value) {
@@ -104,13 +59,7 @@ const OrderSideActorContainer = ({header, sizeCosts, costs, additionalToppingsCo
 
             <div className={styles.pizzaAllIngridients__toppingsList}>
                 {additionalToppingsContent}
-                <ToppingsList
-                    currentCost={currentToppingsCost}
-                    setCurrentCost={handleCurrentToppingsCost}
-                    toppings={toppings}
-                    addSelected={addSelectedTopping}
-                    removeSelected={removeSelectedTopping}
-                ></ToppingsList>
+                <ToppingListCalculator></ToppingListCalculator>
             </div>
 
             <div className={styles.pizzaAllIngridients__price}>
--- frontend/pizza-site/src/components/pizzaOrderPopup/PizzaOrderPopup.jsx
+++ frontend/pizza-site/src/components/pizzaOrderPopup/PizzaOrderPopup.jsx
@@ -6,11 +6,13 @@ import styles from "./PizzaOrderPopup.module.css";
 export const PriseContext = createContext();
 export const ToppingsContext = createContext();
 export const CurrentSizeContext = createContext();
+export const ToppingsCostContext = createContext();
 
 const PizzaOrderPopup = ({ isActive, setPopupState, action, pizzaId }) => {
   const [finalPrise, setFinalPrise] = useState(0);
   const [selectedToppings, setSelectedToppings ] = useState({});
   const [currentSize, setCurrentSize ] = useState();
+  const [currentToppingsCost, setCurrentToppingsCost] = useState(0);
 
   if (!isActive) {
     return <div></div>;
@@ -20,13 +22,15 @@ const PizzaOrderPopup = ({ isActive, setPopupState, action, pizzaId }) => {
     <PriseContext.Provider value = {{finalPrise, setFinalPrise}}>
     <ToppingsContext.Provider value={{selectedToppings, setSelectedToppings}}>
     <CurrentSizeContext.Provider value = {{currentSize, setCurrentSize}}>
+      <ToppingsCostContext.Provider value={{currentToppingsCost, setCurrentToppingsCost}}>
           <CustomPopup isActive={isActive} setPopupState={setPopupState}>
-            {action === "default" ? (
-              <DefaultOrderPizzaMenu pizzaId={pizzaId} />
-            ) : action === "custom" ? (
-              <div>Custom order menu</div>
-            ) : null}
-          </CustomPopup>
+              {action === "default" ? (
+                <DefaultOrderPizzaMenu pizzaId={pizzaId} />
+              ) : action === "custom" ? (
+                <div>Custom order menu</div>
+              ) : null}
+            </CustomPopup>
+          </ToppingsCostContext.Provider>
         </CurrentSizeContext.Provider>
         </ToppingsContext.Provider>
       </PriseContext.Provider>
